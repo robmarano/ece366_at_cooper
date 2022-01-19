2e324fcec420ea3c38e2311a7956d53c0cd8a347
Rob Marano <robmarano@gmail.com>
2022-01-19 01:01:55 -0500

    Initial Java code commit

---
+++ build.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+APPS="single-node-threaded-app dist-image"
+
+# First ensure dependencies loaded since .m2 may be empty
+mvn dependency:tree -Ddetail=true
+mvn help:evaluate -Dexpression=project.version
+
+# Clean repo from builds
+./clean.sh
+
+#
+# Config
+#
+APP_VERSION_FILE=./app.version
+
+THEUSER=$(/usr/bin/whoami)
+NOW=$(date "+%Y%m%d%H%M%S")
+APP_MAVEN_VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -e '^[^\[]')
+APP_GIT_VERSION=$(git rev-parse --abbrev-ref HEAD)
+
+APP_VERSION=${APP_MAVEN_VERSION}-${APP_GIT_VERSION}_${NOW}_${THEUSER}
+echo ${APP_VERSION} > ${APP_VERSION_FILE}
+
+echo "LOCALLY building runtime to local folder: ./build ..."
+echo "Version = ${APP_VERSION}"
+
+if [ ! -f "${APP_VERSION_FILE}" ]; then
+    echo "APP Version file DOES NOT exist. CANNOT proceed with build."
+    exit 1
+fi
+
+#
+# Config
+#
+APP_VERSION=$(cat ${APP_VERSION_FILE})
+
+#mvn test
+mvn install -Dmaven.test.skip=true
+mvn package -N -P classpath-deps -Dmaven.test.skip=true
+
+# Package runtimes with compiled and built libraries
+for APP in $APPS
+do
+	echo "Building $APP ..."
+	cd ./$APP
+	./build_app.sh
+	echo "Built $APP"
+	cd ..
+done
+
+#echo "Copying job scripts for at scheduling to build/app/ ..."
+#cp ./cicd/deploy.sh ./build/app/
+#cp ./app/opt/start_app.sh ./build/app/
+#echo "Done."
+
+echo "Done building APP."
